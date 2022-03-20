67aefb00226d57e306d92b7e1bfa9ad255f3e8cf
cryswerton <cryswertons@gmail.com>
2022-03-20 13:07:12 -0300

    created links to navigate through pages as well as their pages

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html')
+
+@app.route("/upload")
+def upload():
+    return render_template('upload.html')
+
+@app.route("/downloads")
+def downloads():
+    return render_template('downloads.html')
